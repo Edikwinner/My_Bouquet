255a573d912e521b4ca556f5b349e1ea65585ec9
Maksim Vishnevskiy <maksvish135@yandex.ru>
2024-03-01 13:35:50 +0300

    Adding new comment to MainActivity.java

--- app/src/main/java/com/example/mybouquet/MainActivity.java
+++ app/src/main/java/com/example/mybouquet/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //New comment
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
